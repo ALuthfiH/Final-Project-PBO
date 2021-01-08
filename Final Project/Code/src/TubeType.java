d2f484a9a600b5e2f6c7da5947e62bf861f56e65
Alfarabi_mzl <74232912+tkayha29@users.noreply.github.com>
2021-01-08 16:29:58 +0700

    Add files via upload

---
+++ Final Project/Code/src/TubeType.java
@@ -0,0 +1,9 @@
+package id.ac.its.alfarabi0039.ridho170.luthfi179.fp_flappybird;
+
+public enum TubeType {
+	 BOTTOM("BOTTOM", 0), 
+	    TOP("TOP", 1);
+	    
+	    private TubeType(final String name, final int ordinal) {
+	    }
+}
