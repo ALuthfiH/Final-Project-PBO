f707c7cec84fa522a7160cb965224658d5f938e7
ridhojagis <75240358+ridhojagis@users.noreply.github.com>
2021-01-12 14:51:44 +0700

    Update Ground.java
    membuat class Ground inheritance terhadap class GameObject

--- Final Project/Code/src/Ground.java
+++ Final Project/Code/src/Ground.java
@@ -1,28 +1,26 @@
 package id.ac.its.alfarabi0039.ridho170.luthfi179.fp_flappybird;
 
-import java.awt.image.ImageObserver;
-import java.awt.Image;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 
-public class Ground {
+public class Ground extends GameObject {
 	private BufferedImage image;
-    private int x1;
     private int x2;
     private float velX;
     
     public Ground() {
-        this.x1 = 0;
+    	super(0);
         this.x2 = 432;
-        this.velX = 3.0f;
+        this.velX = 4.0f;
         this.image = GraphicsLoader.loadGraphics("ground.png");
     }
     
+    @Override
     public void tick() {
-        this.x1 -= (int)this.velX;
+        this.x -= (int)this.velX;
         this.x2 -= (int)this.velX;
-        if (this.x1 + 432 < 0) {
-            this.x1 = 432;
+        if (this.x + 432 < 0) {
+            this.x = 432;
         }
         if (this.x2 + 432 < 0) {
             this.x2 = 432;
@@ -30,7 +28,7 @@ public class Ground {
     }
     
     public void render(final Graphics g) {
-        g.drawImage(this.image, this.x1, 600, null);
+        g.drawImage(this.image, this.x, 600, null);
         g.drawImage(this.image, this.x2, 600, null);
     }
 }
