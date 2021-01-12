57f018845d00a4bcef4b2a53252e60e3329bd213
ridhojagis <75240358+ridhojagis@users.noreply.github.com>
2021-01-12 14:41:33 +0700

    Add files via upload

---
+++ Final Project/Code/src/Main.java
@@ -0,0 +1,9 @@
+package id.ac.its.alfarabi039_ridho170_luthfi179.fp_flappybird;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		new Window(432, 768, "FlappyBird", new Game());
+	}
+}
